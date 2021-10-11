987eead6c7dce96e311cd90cc7cc8144c1e700dd
Fiston-pro <byiringiroetienne2@gmail.com>
2021-10-11 11:20:41 +0200

    Updates

---
+++ precache-manifest.ab2b99812cdff1492548507b2a6f317a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7e68c3e2c49e86344a8e0e7e33ad84",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "a7308ec96de6069393e8",
+    "url": "/personal-website/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f3f598f87d9389fe0f50",
+    "url": "/personal-website/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "a7308ec96de6069393e8",
+    "url": "/personal-website/static/js/2.5cd6b0aa.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/personal-website/static/js/2.5cd6b0aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f598f87d9389fe0f50",
+    "url": "/personal-website/static/js/main.c4709e31.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  }
+]);
