a1c255685445b5c0fa5ed4dea1be63894e5f3c75
Fiston-pro <byiringiroetienne2@gmail.com>
2022-09-30 12:59:14 +0200

    rebuild the website

---
+++ precache-manifest.482cbd81e1da1f66336717d4ccf70481.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e043f90eef414da548b3a112b5ca9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47a292df016f1798a0f5",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "bdd06d712efb180aa705",
+    "url": "/static/css/main.aef8e091.chunk.css"
+  },
+  {
+    "revision": "47a292df016f1798a0f5",
+    "url": "/static/js/2.628edb34.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "/static/js/2.628edb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd06d712efb180aa705",
+    "url": "/static/js/main.3a0b1250.chunk.js"
+  },
+  {
+    "revision": "a359b7bef99ad716f2cb",
+    "url": "/static/js/runtime-main.f1ecf2b3.js"
+  },
+  {
+    "revision": "80e78c143df31b05635243acf344c6db",
+    "url": "/static/media/ISHIMWE CV.80e78c14.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Website/precache-manifest.a5758e3e0faa7439c08b444ab8d6d791.js"
+  "/precache-manifest.482cbd81e1da1f66336717d4ccf70481.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
